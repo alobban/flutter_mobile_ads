aac4e570c5f72acada636346d2648dcc148a70ae
Andrew Lobban <andrewglobban@gmail.com>
2024-03-15 16:21:04 -0500

    Create My AdMob widget

---
+++ lib/my_admob.dart
@@ -0,0 +1,128 @@
+import 'package:ad_mob_test/helpers/ad_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class MyAdmob extends StatefulWidget {
+  const MyAdmob({super.key});
+
+  @override
+  State<MyAdmob> createState() => _MyAdmobState();
+}
+
+class _MyAdmobState extends State<MyAdmob> {
+  late BannerAd _bottomBannerAd;
+  bool _isBottomBannerAdLoaded = false;
+
+  InterstitialAd? _interstitialAd;
+  int _numInterstitialLoadAttempts = 0;
+  int maxFailedLoadAttempts = 3;
+
+  static AdRequest request = const AdRequest(
+    keywords: <String>['foo', 'bar'],
+    contentUrl: 'http://foo.com/bar.html',
+    nonPersonalizedAds: true,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    // MobileAds.instance
+    // .updateRequestConfiguration(RequestConfiguration(testDeviceIds: []));
+    _createInterstitialAd();
+    _createBottomBannerAd();
+  }
+
+  void _createInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: AdHelper.interstitialAdUnitId,
+      request: request,
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad) {
+          print('$ad loaded');
+          _interstitialAd = ad;
+          _numInterstitialLoadAttempts += 1;
+          _interstitialAd = null;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          print('InterstitialAd failed to load $error.');
+          _numInterstitialLoadAttempts += 1;
+          _interstitialAd = null;
+          if (_numInterstitialLoadAttempts < maxFailedLoadAttempts) {
+            _createInterstitialAd();
+          }
+        },
+      ),
+    );
+  }
+
+  void _showInterstitialAd() {
+    if (_interstitialAd == null) {
+      print('Warning: attempt to show interstitial before loaded.');
+      return;
+    }
+
+    _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (InterstitialAd ad) =>
+          {print('ad onAdShowedFullScreenContent.')},
+      onAdDismissedFullScreenContent: (InterstitialAd ad) => {
+        print('$ad onAdDismissedFullScreenContent.'),
+        ad.dispose(),
+        _createInterstitialAd(),
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) => {
+        print('$ad onAdFailedToShowFullScreenContent: $error'),
+        ad.dispose(),
+        _createInterstitialAd(),
+      },
+    );
+    _interstitialAd!.show();
+    _interstitialAd = null;
+  }
+
+  void _createBottomBannerAd() {
+    _bottomBannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBottomBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) => ad.dispose(),
+      ),
+    );
+    _bottomBannerAd.load();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bottomBannerAd.dispose();
+    _interstitialAd?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: _showInterstitialAd,
+            child: const Text('Show Interstitial Ads'),
+          ),
+        ),
+        bottomNavigationBar: _isBottomBannerAdLoaded
+            ? SizedBox(
+                height: _bottomBannerAd.size.height.toDouble(),
+                width: _bottomBannerAd.size.width.toDouble(),
+                child: AdWidget(
+                  ad: _bottomBannerAd,
+                ),
+              )
+            : null,
+      ),
+    );
+  }
+}
